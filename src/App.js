5be26b0b07cd45426628e830f1cbb5f858de616a
stevebiczyk <stevebiczyk@gmail.com>
2023-10-27 23:34:06 +0100

    added NavBar, Hero and Skills sections

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
 import "./App.css";
 import styled, { ThemeProvider } from "styled-components";
 import { darkTheme, lightTheme } from "./utils/Themes.js";
+import NavBar from "./components/NavBar";
+import Hero from "./components/HeroSection";
+import Skills from "./components/SkillsSection";
 
 const Body = styled.div`
   background-color: ${({ theme }) => theme.bg};
   width: 100%;
+  height: 100%;
   overflow-x: hidden;
 `;
 
 function App() {
   return (
     <ThemeProvider theme={(darkTheme, lightTheme)}>
-      <Body></Body>
+      <NavBar />
+      <Body>
+        <Hero />
+        <Skills />
+      </Body>
     </ThemeProvider>
   );
 }

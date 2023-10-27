14393ee2917727b2b2354c1d2b1acc983de32d3d
stevebiczyk <stevebiczyk@gmail.com>
2023-10-27 23:22:35 +0100

    added styling and themes to body

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import "./App.css";
+import styled, { ThemeProvider } from "styled-components";
+import { darkTheme, lightTheme } from "./utils/Themes.js";
+
+const Body = styled.div`
+  background-color: ${({ theme }) => theme.bg};
+  width: 100%;
+  overflow-x: hidden;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <h1>Portfolio</h1>
-    </div>
+    <ThemeProvider theme={(darkTheme, lightTheme)}>
+      <Body></Body>
+    </ThemeProvider>
   );
 }
 

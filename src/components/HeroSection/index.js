bbe558c380bf5bbae25420070f98f9382f019a4d
stevebiczyk <stevebiczyk@gmail.com>
2023-11-05 00:20:22 +0000

    added styling to hero section

--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -1,12 +1,104 @@
 import React from "react";
 import styled from "styled-components";
 
+const HeroContainer = styled.div`
+  background: ${({ theme }) => theme.card_light};
+  display: flex;
+  justify-content: center;
+  position: relative;
+  padding: 80px 30px;
+  @media (max-width: 960px) {
+    padding: 66px 16px;
+  }
+  @media (max-width: 640) {
+    padding: 32px 16px;
+  }
+  z-index: 1;
+
+  clip-path: polygon(0 0, 100% 0, 100% 100%, 70% 95%, 0 100%);
+`;
+
+const HeroBg = styled.div`
+  position: absolute;
+  display: flex;
+  justify-content: end;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  max-width: 1360px;
+  overflow: hidden;
+  padding: 0 30px;
+  top: 50%;
+  left: 50%;
+  -webkit-transform: translateX(-50%) translateY(-50%);
+  transform: translateX(-50%) translateY(-50%);
+
+  @media (max-width: 960px) {
+    justify-content: center;
+    padding: 0 0px;
+  }
+`;
+
+const HeroInnerContainer = styled.div`
+  position: relative;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  max-width: 1100px;
+
+  @media (max-width: 960px) {
+    flex-direction: column;
+  }
+`;
+
+const HeroLeftContainer = styled.div`
+  width: 100%;
+  order: 1;
+  @media (max-width: 960px) {
+    order: 2;
+    margin-bottom: 30px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+
+  @media (max-width: 640px) {
+    order: 2;
+    margin-bottom: 30px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+`;
+
+const HeroRightContainer = styled.div`
+  width: 100%;
+  display: flex;
+  order: 2;
+  justify-content: end;
+  gap: 12px;
+  @media (max-width: 960px) {
+    order: 1;
+    justify-content: center;
+    align-items: center;
+    margin-bottom: 80px;
+  }
+
+  @media (max-width: 640px) {
+    margin-bottom: 30px;
+  }
+`;
+
 const Hero = () => {
   return (
     <div id="about">
       <HeroContainer>
         <HeroBg></HeroBg>
-        <HeroKInnerContainer>
+        <HeroInnerContainer>
           <HeroLeftContainer>
             <HeroContent>
               <HeroH1>Hi, I'm</HeroH1>
@@ -18,7 +110,7 @@ const Hero = () => {
             </HeroContent>
           </HeroLeftContainer>
           <HeroRightContainer></HeroRightContainer>
-        </HeroKInnerContainer>
+        </HeroInnerContainer>
       </HeroContainer>
     </div>
   );

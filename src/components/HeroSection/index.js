bdb1899968c6cdeabd3eb8fc2786cc80b873bcfd
stevebiczyk <stevebiczyk@gmail.com>
2023-11-05 00:02:39 +0000

    created hero section structure

--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -1,7 +1,27 @@
 import React from "react";
+import styled from "styled-components";
 
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <div id="about">
+      <HeroContainer>
+        <HeroBg></HeroBg>
+        <HeroKInnerContainer>
+          <HeroLeftContainer>
+            <HeroContent>
+              <HeroH1>Hi, I'm</HeroH1>
+              <HeroName>Istvan</HeroName>
+              <HeroP>
+                I'm a Full Stack Developer. I love to build and design web
+                applications.
+              </HeroP>
+            </HeroContent>
+          </HeroLeftContainer>
+          <HeroRightContainer></HeroRightContainer>
+        </HeroKInnerContainer>
+      </HeroContainer>
+    </div>
+  );
 };
 
 export default Hero;

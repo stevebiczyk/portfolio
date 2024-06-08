917d12bdad93dd033262ed4dd50c8f19cd2c6ecd
stevebiczyk <stevebiczyk@gmail.com>
2024-06-08 17:05:56 +0100

    added projects section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import "./App.css";
 import styled, { ThemeProvider } from "styled-components";
 import { darkTheme, lightTheme } from "./utils/Themes.js";
-import NavBar from "./components/NavBar";
-import Hero from "./components/HeroSection";
-import Skills from "./components/sections/Skills.jsx";
-import Education from "./components/Education";
-import Experience from "./components/sections/Experience.jsx";
+import NavBar from "./components/sections/NavBar.jsx";
+import Hero from "./components/sections/Hero";
+import Skills from "./components/sections/Skills";
+import Education from "./components/sections/Education";
+import Experience from "./components/sections/Experience";
+import Projects from "./components/sections/Projects.jsx";
 import { BrowserRouter as Router } from "react-router-dom";
 
 const Body = styled.div`
